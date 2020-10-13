8f898d2d7b64107e9159f50dd249d2138dd8de17
Владимир <51334865+wingofnight@users.noreply.github.com>
2020-10-13 08:38:03 +0300

    Create Примеры цветов.cs

---
+++ Примеры цветов.cs
@@ -0,0 +1,49 @@
+   тут есть примеры цветов, но вообще, кошда набираешь ConsoleColor и ставишь точку, то там появляется список цветов.             
+                
+                case "Blue":
+                    ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "DarkBlue":
+                    ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "Pink":
+                    ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case "DarkPink":
+                    ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "Gray":
+                    ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case "DarkGray":
+                    ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "White":
+                    ForegroundColor = ConsoleColor.White;
+                    break;
+                case "Read":
+                    ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "DarkRead":
+                    ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "DarkGreen":
+                    ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "Green":
+                    ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "Yellow":
+                    ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "DarkYellow":
+                    ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "Cyan":
+                    ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case "DarkCyan":
+                    ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                default:
+                    break;
