e1a6587f6c39c97cea599fff5d301b7b7ed2230b
Владимир <51334865+wingofnight@users.noreply.github.com>
2020-10-13 08:22:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace case_newby_lvl_1
+{
+    public class ColorProfile
+    {
+        public ConsoleColor BackGround { get; set; }
+        public ConsoleColor ForeGround { get; set; }
+    }
+    public class SectorPaint
+    {
+        //int lastLenght = 0;
+        string symbol = "";//нужно сделать конструктор класса, который задавал бы значение symbol и запускал функцию ClassesCreate()
+        private void ClassesCreate()
+        {
+            //нужно сделать все цвета которые есть в ConsoleColor.блаблабла. 
+            ColorProfile Red = new ColorProfile();
+            Red.ForeGround = ConsoleColor.Red;
+            Red.BackGround = ConsoleColor.Black;
+
+            ColorProfile DarckRed = new ColorProfile();
+            DarckRed.ForeGround = ConsoleColor.DarkRed;
+            DarckRed.BackGround = ConsoleColor.Black;
+
+        }
+
+        public void Point(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+    }
